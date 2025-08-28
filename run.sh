8d0cde754e14d2f48beb28cdb353d5cff8d8bc89
Caio <caiombaraujo@gmail.com>
2025-08-27 23:53:17 -0300

    fix root user action run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install dependencies
-pip install --no-cache-dir --root-user-action -r requirements.txt
+pip install --no-cache-dir --root-user-action=ignore -r requirements.txt
 
 # run the main application
 exec uvicorn main:app --host 0.0.0.0 --port 80 --reload
