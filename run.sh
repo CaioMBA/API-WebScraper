32dfcc0139cefb2145afa45fea58015081526a61
Caio <caiombaraujo@gmail.com>
2025-08-27 23:46:34 -0300

    add root user action to run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 set -e
 
 # install dependencies
-pip install --no-cache-dir -r requirements.txt
+pip install --no-cache-dir --root-user-action -r requirements.txt
 
 # run the main application
 exec uvicorn main:app --host 0.0.0.0 --port 80 --reload
